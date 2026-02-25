263518c3f64e4e12cdb28e34fa52809a587f6181
Vad-cod <atenciodiazvalentina@gmail.com>
2026-02-25 09:01:57 -0500

    Funciones

---
+++ main.py
@@ -0,0 +1,17 @@
+def suma(num1, num2):
+    print(f"Suma: {num1 + num2}")
+
+def resta(num1, num2):
+    print(f"Resta: {num1 - num2}")
+
+def multiplicacion(num1, num2):
+    print(f"Multiplicacion: {num1 + num2}")
+
+def division(num1, num2):
+    print(f"Division: {num1 / num2}")
+
+
+suma(1, 2)
+resta(10, 20)
+multiplicacion(3, 5)
+division(10, 2)
