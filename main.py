8baf742ee145f6bf1a1d5ce0390ad1326fc853bf
Vad-cod <atenciodiazvalentina@gmail.com>
2026-02-25 08:29:55 -0500

    Suma y resta

---
+++ main.py
@@ -0,0 +1,8 @@
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+print(f"\n2Addition: {addition}")
+print(f"Subtraction: {subtraction}")
